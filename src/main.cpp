4776fe56c717ae3a98138ac78d527d111619180b
JLS666 <43670180+JLS666@users.noreply.github.com>
2018-09-29 17:08:51 +0200

    ergrege
    fdsfsfe

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ void setup()
 
 void loop() 
 {
-    Serial.println("Tesdsdt");
-    Serial.println("Test2");
+    Serial.println("gregr");
+    Serial.println("fdsfs");
+	Serial.println("thrhrsssssster");
 }
