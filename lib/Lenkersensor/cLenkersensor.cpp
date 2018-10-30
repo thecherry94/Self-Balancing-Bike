e24e06517658897b3f0e1307fe5a3468e0076aa7
JLS666 <43670180+JLS666@users.noreply.github.com>
2018-10-30 21:19:57 +0100

    Logik Quicksort in cpp gemacht

--- lib/Lenkersensor/cLenkersensor.cpp
+++ lib/Lenkersensor/cLenkersensor.cpp
@@ -2,6 +2,30 @@
 #include "cLenkersensor.h"
 
 ///////// cLenkersensor.cpp /////////////////
+void quickSort(float  arr[], int left, int right) { // Quelle: http://www.algolist.net/Algorithms/Sorting/Quicksort
+  int i = left, j = right;
+  float tmp;
+  float pivot = arr[(left + right) / 2];
+  /* partition */
+  while (i <= j) {
+    while (arr[i] < pivot)
+      i++;
+    while (arr[j] > pivot)
+      j--;
+    if (i <= j) {
+      tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+      i++;
+      j--;
+    }
+  };
+  /* recursion */
+  if (left < j)
+    quickSort(arr, left, j);
+  if (i < right)
+    quickSort(arr, i, right);
+}
 cLenkersensor::cLenkersensor()
 {
   lastLenkwinkel = analogRead(pinPoti) * (-0.083) + 170;
