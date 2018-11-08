4dd6e6d87eae95f66df78c9aafe340bb7a2fcdb1
JLS666 <43670180+JLS666@users.noreply.github.com>
2018-11-08 08:58:02 +0100

    finale Pins

--- lib/Lenkersensor/cLenkersensor.h
+++ lib/Lenkersensor/cLenkersensor.h
@@ -6,9 +6,9 @@
 #include "driver/pcnt.h"
 
 ////////////// Lenkersensor global.h //////////////
-#define ENCODER_INPUT 22             // Pin Counter Input Definition
-#define ENCODER_DIRECTION 21         // Pin Control Input Definition
-#define ENCODER_ZERO 23             // Pin Nulldurchgang Definition
+#define ENCODER_INPUT 13             // Pin Counter Input Definition
+#define ENCODER_DIRECTION 12         // Pin Control Input Definition
+#define ENCODER_ZERO 14             // Pin Nulldurchgang Definition
 struct lenkerDaten  // Struct mit Daten für den Lenker
 {
   float lenkwinkel; // Winkel des Lenkers in °, in der Mitte: 0, nach links: positv, nach rechts: negativ
