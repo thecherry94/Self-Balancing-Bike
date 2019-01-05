4e55f964d040e87eb56c54987e653a42218c6a51
AML <43120372+AML113@users.noreply.github.com>
2019-01-05 15:27:31 +0100

    Sicherung

---
+++ lib/Lenkersensor/cLenkersensor_alt.cpp
@@ -0,0 +1,142 @@
+#include <Arduino.h>                // damit millis und analogWrite geht.
+#include "cLenkersensor.h"
+
+
+bool lenkerflag = 1;
+pcnt_config_t config_pcnt;
+int16_t counter;
+
+cLenkersensor::cLenkersensor()
+{
+    
+    //abs(7);
+      //Setup Lenkersensor
+    pinMode(ENCODER_INPUT, INPUT);
+    pinMode(ENCODER_DIRECTION, INPUT);
+    pinMode(ENCODER_ZERO, INPUT);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_ZERO), isr_lenkersensor, RISING);
+    
+    lastLenkwinkel = 0;
+    lastLenkgeschwindigkeit = 0;
+    lastZeit = esp_timer_get_time()*1000;
+    daten.lenkbeschleunigung = 0;
+    daten.lenkgeschwindigkeit = 0;
+    daten.lenkwinkel = 0;
+    
+    // Set PCNT input signal and control GPIOs
+    config_pcnt.pulse_gpio_num = ENCODER_INPUT;
+    config_pcnt.ctrl_gpio_num = ENCODER_DIRECTION;
+    config_pcnt.channel = PCNT_CHANNEL_0,
+    config_pcnt.unit = ENCODER_1;
+    // What to do on the positive / negative edge of pulse input?
+    config_pcnt.pos_mode = PCNT_COUNT_INC;   // Count up on the positive edge
+    config_pcnt.neg_mode = PCNT_COUNT_DIS;   // Keep the counter value on the negative edge PCNT_COUNT_DEC
+    // What to do when control input is low or high?
+    config_pcnt.lctrl_mode = PCNT_MODE_KEEP; // Reverse counting direction if low
+    config_pcnt.hctrl_mode = PCNT_MODE_REVERSE; // Keep the primary counter mode if high
+    // // Set the maximum and minimum limit values to watch
+    config_pcnt.counter_h_lim = ENCODER_MAX_VALUE;
+    config_pcnt.counter_l_lim = ENCODER_MIN_VALUE;
+    
+    pcnt_unit_config(&config_pcnt);
+    pcnt_counter_pause(ENCODER_1);
+    pcnt_counter_clear(ENCODER_1);
+}
+int zaehler2 = 0;
+void cLenkersensor::readCounter()
+{
+
+    pcnt_get_counter_value(ENCODER_1, &counter);
+    daten.lenkwinkel = 2*360/511.0*counter*9/28.0+UMRECHNUNGSZAHL;  // Für Lenkwinkel des Lenkers
+    if(millis() != lastZeit)        // Sicherheitsfunktion um eine durch 0 Teilung zu verhindern
+    {
+        daten.lenkgeschwindigkeit = (daten.lenkwinkel - lastLenkwinkel) / (millis() - lastZeit) * 1000; // [°/s]
+        daten.lenkbeschleunigung = (daten.lenkgeschwindigkeit - lastLenkgeschwindigkeit) / (millis() - lastZeit) * 1000; // [°/s²]
+        //Serial.println(" Ich rechne");
+        //Serial.println(daten.lenkgeschwindigkeit);
+
+    }
+    if(zaehler2 > 99)
+    {
+        
+         printf("%lu;%f;", millis(), daten.lenkwinkel);
+         zaehler2 = 0;
+    }
+    lastLenkwinkel = daten.lenkwinkel;
+    lastLenkgeschwindigkeit = daten.lenkgeschwindigkeit;
+    lastZeit = millis();
+    zaehler2++;
+    
+
+   
+}
+
+float cLenkersensor::getMotorwinkel()
+{
+    return counter*360/511;                             // Für 360° Ausgabe
+}
+
+bool cLenkersensor::getData(lenkerDaten &pdaten)
+{
+    pdaten = daten;     // Daten rückgeben
+    if (daten.lenkwinkel >= 90 || daten.lenkwinkel <= -90 || lenkerflag == 1)
+        return 1;
+    else 
+        return 0;
+}
+
+float cLenkersensor::getLenkerwinkel()
+{
+    if (daten.lenkwinkel >= 90 || daten.lenkwinkel <= -90 || lenkerflag == 1)
+    {
+        return 666;
+    }
+    else 
+    {
+        return daten.lenkwinkel;     // Daten rückgeben
+    }
+       
+}
+
+float cLenkersensor::getLenkergeschwindigkeit()
+{
+    if (daten.lenkwinkel >= 90 || daten.lenkwinkel <= -90 || lenkerflag == 1)
+    {
+        return 666;
+    }
+    else
+    {
+        return daten.lenkgeschwindigkeit;
+    }
+}
+
+float cLenkersensor::getLenkerbeschleunigung()
+{
+    
+    if (daten.lenkwinkel >= 90 || daten.lenkwinkel <= -90 || lenkerflag == 1)
+    {
+        return 666;
+    }
+    else 
+    {
+        return daten.lenkbeschleunigung;     // Daten rückgeben
+        
+    }
+        
+}
+
+bool cLenkersensor::getCalibration()
+{
+    if(lenkerflag == 1)
+        return 1;
+    else
+        return 0;
+}
+
+void isr_lenkersensor()
+{
+    lenkerflag = 0;
+    pcnt_counter_resume(ENCODER_1);
+    pcnt_counter_clear(ENCODER_1);
+    //detachInterrupt(ENCODER_ZERO);
+}
---
+++ lib/Lenkersensor/cLenkersensor_alt.h
@@ -0,0 +1,66 @@
+/*  Autor: Julian Schweizerhof (Till Gostner)
+    Name: cLenksensor.h */
+
+#ifndef LENKERSENSOR__H
+#define LENKERSENSOR__H
+#include "driver/pcnt.h"
+
+////////////// Lenkersensor global.h //////////////
+#define ENCODER_INPUT 15             // Pin Counter Input Definition
+#define ENCODER_DIRECTION 4         // Pin Control Input Definition
+#define ENCODER_ZERO 5             // Pin Nulldurchgang Definition
+struct lenkerDaten  // Struct mit Daten für den Lenker
+{
+  float lenkwinkel; // Winkel des Lenkers in °, in der Mitte: 0, nach links: positv, nach rechts: negativ
+  float lenkgeschwindigkeit;  // Winkelgeschwindigkeit in °/s, nach links: positv, nach rechts: negativ
+  float lenkbeschleunigung; // Winkelbeschleunigung °/s², nach rechts: positv, nach rechts: negativ
+};
+////////////////////////////////////////////////////
+
+///////// cLenkersensor.h /////////////////
+
+#define ENCODER_1 PCNT_UNIT_0       // Name vom ENCODER_1
+#define ENCODER_MAX_VALUE 511       // max Zählerwert vom Counter
+#define ENCODER_MIN_VALUE -511      // min Zählerwert vom Counter
+#define UMRECHNUNGSZAHL -6.9           // wenn der Nullpunkt nicht in der Mitte ist
+#define GENAUIGKEIT 1               // Nach wie vielen Millisekunden wird die Geschwindigkeit neu berechnet?
+void isr_lenkersensor();            // isr beim Nulldurchgang
+
+class cLenkersensor
+{
+  private: 
+    lenkerDaten daten;  // Strukt für die Lenkerdaten mit Winkel, Geschwindigkeit, Beschleunigung
+    float lastLenkwinkel; // letzter Lenkwenkel in °, für die Berechnung der Geschwindigkeit 
+    float lastLenkgeschwindigkeit;  // letzter Lenkwenkel in °/s, für die Berechnung der Beschleunigung
+    unsigned int lastZeit;  // letze Zeit des letzen Messwertes in ms
+   public:
+    cLenkersensor();  // Konstruktur
+    void readCounter(); // muss ausgeführt werden, damit die Messwerte kommen
+
+    float getLenkerwinkel();    // return Motorwinkel als float
+    float getLenkergeschwindigkeit();
+    float getLenkerbeschleunigung();
+    bool getData(lenkerDaten &pdaten);  // Messdaten bekommen, Übergabeparameter: Datenstruct vom Typ lenkerDaten, Rückgabewert: 0: alles okay, 1: Lenkermotorbetrieb verboten!!!
+    float getMotorwinkel(); // get Motorwinkel in °
+    bool getCalibration();
+};
+
+////// Beispiel Implementierung ////
+/*
+
+cLenkersensor Lenkersensor;         // Objekt des Lenkersensor
+lenkerDaten lDaten;                 // Struct von lenkerDaten
+void setup() {
+    pinMode(ENCODER_INPUT, INPUT);
+    pinMode(ENCODER_DIRECTION, INPUT);
+    pinMode(ENCODER_ZERO, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_ZERO), isr_lenkersensor, RISING);
+    Serial.begin(115200);
+}
+void loop() {
+    Lenkersensor.readCounter();
+    Lenkersensor.getData(lDaten);
+    Serial.printf("%f;%f;%f\n", lDaten.lenkwinkel, lDaten.lenkgeschwindigkeit, lDaten.lenkbeschleunigung); 
+}
+*/
+#endif
